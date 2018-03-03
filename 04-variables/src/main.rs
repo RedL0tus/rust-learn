8e1ee5ba112b9b1fb3b2abfd0c3a212d88ee6d22
Kay <i@v2bv.net>
2018-03-03 13:07:12 -0800

    Change comments

--- 04-variables/src/main.rs
+++ 04-variables/src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
-    /* Mutability */
+    // Mutability
     let mut x = 5; // mut means mutable.
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
     
-    /* Consts */
+    // Consts
     const EXAMPLE_CONST: u32 = 100_000; // type must be annotated
     println!("The value of the example const is: {}", EXAMPLE_CONST);
 
-    /* Shadowing */
+    // Shadowing
     let x = 5;
     let x = x + 1;
     let x = x + 2; // This is what we will see when we use it.
@@ -19,9 +19,7 @@ fn main() {
     let spaces = spaces.len(); // Shadowing can change data type
     println!("The value of spaces is: {}", spaces);
 
-    /*
-     * let mut spaces = "     ";
-     * spaces = space.len();
-     * will result in an error.
-     */
+    // let mut spaces = "     ";
+    // spaces = space.len();
+    // will result in an error.
 }
