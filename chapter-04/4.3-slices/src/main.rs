9fd01c00c1e89b291d37b33337d9d791f7e1772c
Kay <i@v2bv.net>
2018-03-05 22:00:03 -0800

    Add 4.3-slices

---
+++ chapter-04/4.3-slices/src/main.rs
@@ -0,0 +1,59 @@
+fn main() {
+    let mut x: String = String::from("Hello, world!");
+    println!("{}", first_word(&x));
+    println!("{},{}", second_word(&x).0, second_word(&x).1);
+    println!("{}", first_word_slice(&x));
+    println!("{}", second_word_slice(&x));
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes(); // Convert String to an array of bytes.
+    for (i, &item) in bytes.iter().enumerate() { // Create an iterator
+        if item == b' ' { 
+            return i;
+        }
+    }
+    return s.len();
+}
+
+fn first_word_slice(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+    return &s[..];
+}
+
+fn second_word(s: &String) -> (usize,usize) {
+    let bytes = s.as_bytes();
+    let mut count: usize = 0;
+    let mut start: usize = 0;
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            count = count + 1;
+            if count == 2 {
+                return (start, i); 
+            }
+            start = i + 1;
+        }
+    }
+    return (start, s.len());
+}
+
+fn second_word_slice(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    let mut count: usize = 0;
+    let mut start: usize = 0;
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            count = count + 1;
+            if count == 2 {
+                return &s[start..i];
+            }
+            start = i + 1;
+        }
+    }
+    return &s[start..];
+}
