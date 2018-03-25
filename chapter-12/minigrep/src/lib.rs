34988a634c8b9e911c3ba3a7493d584155461d52
Kay <i@v2bv.net>
2018-03-24 21:02:38 -0700

    12.5-working_with_environment_variables

--- chapter-12/minigrep/src/lib.rs
+++ chapter-12/minigrep/src/lib.rs
@@ -1,19 +1,23 @@
+use std::env;
 use std::fs::File;
 use std::error::Error;
 use std::io::prelude::*;
 
 pub struct Config {
-    query: String,
-    filename: String,
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() == 3 {
+            let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
             return Ok(Config {
                 query: args[1].clone(),
                 filename: args[2].clone(),
-            })
+                case_sensitive,
+            });
         } else if args.len() == 1 {
             return Err("Usage: <query> <filename>");
         } else {
@@ -23,11 +27,16 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut f = File::open(config.filename).expect("File not found.");
+    let mut f = File::open(config.filename)?;
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("Error while reading the file.");
-    for line in search(&config.query, &contents) {
+    f.read_to_string(&mut contents)?;
+    let mut results = Vec::new();
+    if config.case_sensitive {
+        results = search(&config.query, &contents);
+    } else {
+        results = search_case_insensitive(&config.query, &contents);
+    };
+    for line in results {
         println!("{}", line);
     }
     return Ok(());
@@ -43,19 +52,43 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     return results;
 }
 
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    return results;
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "Salted";
         let contents = "\
 Poisonous Salted Fish
 Can't be used as food.
-100 BTC per serve only at fishmart!";
+100 BTC per serve, only at fishmart!";
         assert_eq!(
             search(query, contents),
+            vec!["Poisonous Salted Fish"]
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "sAlTeD";
+        let contents = "\
+Poisonous Salted Fish
+Can't be used as food.
+100 BTC per serve, only at fishmart!";
+        assert_eq!(
+            search_case_insensitive(query, contents),
             vec!["Poisonous Salted Fish"]
         );
     }
