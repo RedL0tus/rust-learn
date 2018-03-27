6b2137a1994720ec5125ed7f5a2fe14140f5f7a7
Kay <i@v2bv.net>
2018-03-26 19:38:02 -0700

    Add 13.1-closure_anonymous_function_that_can_capture_their_environment

---
+++ chapter-13/13.1-closure_anonymous_function_that_can_capture_their_environment/src/main.rs
@@ -0,0 +1,89 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    );
+
+    // Closure Type Inference and Annotation
+    fn  add_one_v1   (x: u32) -> u32 { x + 1 }
+    let add_one_v2 = |x: u32| -> u32 { x + 1 };
+    let add_one_v3 = |x|             { x + 1 };
+    let add_one_v4 = |x|               x + 1  ;
+    println!("{}", add_one_v1(1));
+    println!("{}", add_one_v2(1));
+    println!("{}", add_one_v3(1));
+    println!("{}", add_one_v4(1));
+
+    // Capturing the Environment with Closure
+    let x = 1;
+    let equal_to_x = |y| y == x;
+    println!("X can still be accessed: x={}.", x);
+    let equal_to_x_move = move |z| z == x;
+    // println!("X cannot be accessed anymore. {}", x);
+    println!("{}", equal_to_x(1));
+    println!("{}", equal_to_x_move(1));
+}
+
+// Storing Closures Using Generic Parameters and the `Fn` Traits
+struct Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cacher<T>
+    where T: Fn(u32) -> u32
+{
+    fn new(calculation: T) -> Cacher<T> {
+        Cacher {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            },
+        }
+    }
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            expensive_result.value(intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            expensive_result.value(intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
+        }
+    }
+}
