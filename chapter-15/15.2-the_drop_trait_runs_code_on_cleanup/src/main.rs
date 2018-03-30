f12c264feda895f1e4049f2e355e62c4486a41c0
Kay Miller <i@ikay.work>
2018-03-30 14:10:46 -0700

    Add 15.2-the_drop_trait_runs_code_on_cleanup

---
+++ chapter-15/15.2-the_drop_trait_runs_code_on_cleanup/src/main.rs
@@ -0,0 +1,23 @@
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+fn main() {
+    println!(">>> Begin of main()");
+    let c = CustomSmartPointer { data: String::from("My stuff") };
+    let d = CustomSmartPointer { data: String::from("Other stuff") };
+    println!(">>> CustomSmartPointers c&d are created.");
+    println!("c has a value of `{}`.", c.data);
+    println!("d has a value of `{}`.", d.data);
+    // Dropping a value with `std::mem::drop`
+    // c.drop(); // Explicit destructor calls not allowed
+    drop(c);
+    println!(">>> CustomSmartPointer c dropped before the end of main.");
+    println!(">>> End of main()");
+}
