18e7f6da8432fb2af463af55257484e9822d28d4
Kay Miller <i@ikay.work>
2018-04-04 13:04:22 -0700

    Add 20.3-designing_the_thread_pool_interface

--- chapter-20/hello/src/main.rs
+++ chapter-20/hello/src/main.rs
@@ -1,5 +1,8 @@
+extern crate hello;
+
 use std::thread;
 use std::fs::File;
+use hello::ThreadPool;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::time::Duration;
@@ -7,9 +10,14 @@ use std::net::TcpListener;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
